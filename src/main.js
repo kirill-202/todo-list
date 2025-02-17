fa1ae7c20c396f3c00d7d9580b38e191b9358e82
Kirill <kirillgoodline@gmail.com>
2025-02-18 03:24:47 +0400

    add new project creation functionality and event listeners

--- src/main.js
+++ src/main.js
@@ -1,2 +1,48 @@
 import "./styles.css";
+import "./form.css";
+import {Project} from "./project-class.js"
 
+let CURRENT_PROJECT = "";
+const PROJECTS = [];
+
+
+
+
+function handleSubmit(event) {
+    event.preventDefault(); 
+
+    const projectName = document.getElementById("projectName").value;
+
+    if (projectName.trim() === "") {
+        alert("Project name cannot be empty!");
+        return;
+    }
+
+    const project = new Project(projectName);
+    PROJECTS.push(project);
+
+    addProjectToNavBar(project);
+    projectModal.close();
+    const form = document.getElementById("createProjectForm");
+    form.reset();  
+}
+
+function addProjectToNavBar(project) {
+
+    const newNavBarProject = document.createElement("div");
+    newNavBarProject.classList.add("nav-project");
+    newNavBarProject.textContent = project.name;
+    newNavBarProject.id = project.id;
+
+    const navBar = document.querySelector(".nav-bar");
+    navBar.appendChild(newNavBarProject);
+}
+
+
+const newProjectButton = document.querySelector(".create-project");
+newProjectButton.addEventListener("click", () => {
+    projectModal.showModal();
+});
+
+const form = document.getElementById("createProjectForm");
+form.addEventListener("submit", handleSubmit);
